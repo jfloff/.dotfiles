cb7ddef0099c544d667b27163ceac9bd4ddc770a
João Loff <jfloff@gmail.com>
2015-10-15 00:26:37 +0100

    supress output

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,13 @@ if [[ ! -e ~/.dotfiles ]]; then
   # install command line tools so we have git
   # loops while we don't have the tools installed
   while true; do
-    xcode-select --install
+    xcode-select --install > /dev/null 2>&1
     sleep 5
     xcode-select -p > /dev/null 2>&1
     [ $? == 2 ] || break
   done
   # clone repo
-  git clone --recurse-submodules https://github.com/jfloff/.dotfiles ~/.dotfiles
+  git clone --recurse-submodules https://github.com/jfloff/.dotfiles ~/.dotfiles > /dev/null 2>&1
 fi
 
 # already exists run install script
