11a4903482d5748f278d38f349dd59cd2b3aa0f2
João Loff <jfloff@gmail.com>
2015-10-14 17:21:32 +0100

    Add MSOffice. Fix extra script

--- extras.sh
+++ extras.sh
@@ -6,13 +6,15 @@ if [[ ! -e ./downloads ]]; then
 fi
 
 ###############################################################################
-bot "Installing Alfred"
+bot "Setting up >Alfred<"
 ###############################################################################
+running "Installing Alfred"
 pushd ./downloads > /dev/null 2>&1
 download https://www.dropbox.com/s/b8bwzhl7trj9rxx/alfred-2.8_414.zip
 unzip -qof alfred-2.8_414.zip
 open alfred-2.8_414
 popd > /dev/null 2>&1
+ok
 
 question "Is Alfred correctly installed? [y|N]" response
 if [[ $response =~ ^(yes|y|Y) ]];then
@@ -20,6 +22,7 @@ if [[ $response =~ ^(yes|y|Y) ]];then
   pushd ~/Library/Application\ Support/Alfred\ 2/ > /dev/null 2>&1
   symlinkifne Alfred.alfredpreferences
   popd > /dev/null 2>&1
+  ok
 
   # running "Installing Workflows"
   # caffeinate
@@ -34,6 +37,7 @@ if [[ $response =~ ^(yes|y|Y) ]];then
   # required_alfred_workflow https://github.com/packal/repository/raw/master/stack_overflow/stackoverflow.alfredworkflow
   # ok;
 fi
+botdone
 
 ###############################################################################
 bot "Downloading custom link software"
@@ -46,7 +50,12 @@ unzip -qof texpad-1716.zip
 # tuxera ntfs
 download https://www.dropbox.com/s/1pp7ai6q9dng33d/tuxera-nfts-2015.zip
 unzip -qof texpad-1716.zip
+# ms office
+download https://www.dropbox.com/s/zr8fzj6ppp40ci9/msoffice-2016-15.14.0.zip
+unzip -qof msoffice-2016-15.14.0.zip
 # tom tom GPS
 download http://cdn.sa.services.tomtom.com/static/sa/Mac/MyDriveConnect.dmg
-
+# open this dir in finder so we can install
+open .
 popd > /dev/null 2>&1
+botdone
