47f9c70336b0b2d2aa56295fd65e7a3685081e8e
João Loff <jfloff@gmail.com>
2015-10-16 14:09:55 +0100

    Keep bootstrap alive during download

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,10 @@
 
 # check if there is a clone of .dotfiles already running
 if [[ ! -e ~/.dotfiles ]]; then
+  # keeps system alive
+  caffeinate &
+  caff_pid = $?
+
   # install command line tools so we have git
   # loops while we don't have the tools installed
   while true; do
@@ -16,6 +20,8 @@ if [[ ! -e ~/.dotfiles ]]; then
   done
   # clone repo
   git clone --recurse-submodules https://github.com/jfloff/.dotfiles ~/.dotfiles > /dev/null 2>&1
+  
+  kill $caff_pid
 fi
 
 # already exists run install script
